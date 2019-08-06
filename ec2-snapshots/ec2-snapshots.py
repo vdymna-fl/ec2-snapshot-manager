ff16b1ad3fba6bbe2795a1b2bdd582bcdf2d4d20
Vitaliy Dymna <vitaliy.dymna@xcentium.com>
2019-08-05 23:04:30 -0500

    List volumes and stop/start instances commands

--- ec2-snapshots/ec2-snapshots.py
+++ ec2-snapshots/ec2-snapshots.py
@@ -1,16 +1,18 @@
 import boto3
 import click
 
-PROJECT = 'project' # make this a constant
+TAG_PROJECT = 'project' # make this a constant
+STOPPED_STATE = 'stopped'
+RUNNING_STATE = 'running'
 
 session = boto3.Session(profile_name='default')
 ec2 = session.resource('ec2')
 
-def get_instances_by_project(project):
+def get_ec2_instances(project):
 	instances = []
 
 	if project:
-		filters = [{ 'Name': 'tag:'+PROJECT, 'Values': [ project ]}]
+		filters = [{ 'Name': 'tag:' + TAG_PROJECT, 'Values': [ project ]}]
 		instances = ec2.instances.filter(Filters=filters)
 	else:
 		instances = ec2.instances.all()
@@ -23,26 +25,71 @@ def cli():
 
 @cli.group('instances')
 def instances():
-	"Commands for instances"
+	"""Commands for instances"""
 
 @instances.command('list')
 @click.option('--project', default=None, help="Only instance for project based on tag")
-def list_ec2_instaces(project):
+def list_instances(project):
 	"""List EC2 instances"""
-	instances = get_instances_by_project(project)
-
-	for instance in instances:
-		tags = { t['Key']: t['Value'] for t in instance.tags or []}
+	
+	for i in get_ec2_instances(project):
+		tags = { t['Key']: t['Value'] for t in i.tags or []}
 
 		print(', '.join([ 
-			instance.id, 
-			instance.instance_type, 
-			instance.placement['AvailabilityZone'], 
-			instance.state['Name'], 
-			instance.public_dns_name,
-			tags.get(PROJECT, '<no project>') ]))
+			i.id, 
+			i.instance_type, 
+			i.placement['AvailabilityZone'], 
+			i.state['Name'], 
+			i.public_dns_name or '<no public dns name>',
+			tags.get(TAG_PROJECT, '<no project>') 
+		]))
 
 	return
-        
+
+@instances.command('start')
+@click.option('--project', default=None, help="Only instance for project based on tag")
+def start_instances(project):
+	"""Start EC2 instances"""
+
+	for i in get_ec2_instances(project):
+		if i.state['Name'] == STOPPED_STATE:
+			print("Starting {0} instance...".format(i.id))
+			i.start()
+		else:
+			print("Skipping {0} instance in {1} state".format(i.id, i.state['Name']))
+
+@instances.command('stop')
+@click.option('--project', default=None, help="Only instance for project based on tag")
+def stop_instances(project):
+	"""Stop EC2 instances"""
+
+	for i in get_ec2_instances(project):
+		if i.state['Name'] == RUNNING_STATE:
+			print("Stopping {0} instance...".format(i.id))
+			i.stop()
+		else:
+			print("Skipping {0} instance in {1} state".format(i.id, i.state['Name']))
+
+
+@cli.group('volumes')
+def volumes():
+	"""Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None, help='Only instance for project based on tag')
+def list_volumes(project):
+	"""List volumes for EC2 instaces"""
+
+	for i in get_ec2_instances(project):
+		for v in i.volumes.all():
+			print(', '.join([
+				v.id,
+				i.id,
+				v.state,
+				str(v.size) + ' GiB',
+				v.encrypted and "Encrypted" or "Not Encrypted"
+			]))
+	return
+      
 if __name__ == '__main__':
 	cli()
